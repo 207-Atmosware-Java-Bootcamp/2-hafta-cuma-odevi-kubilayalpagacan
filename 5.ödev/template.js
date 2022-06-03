f22e6c69a257d8e1b4250e5b68f113424d71cfd7
Kubilay Alp Ağacan <91961953+kubilayalpagacan@users.noreply.github.com>
2022-06-03 16:34:22 +0300

    2.Hafta-5.Ödev

---
+++ 5.ödev/template.js
@@ -0,0 +1,15 @@
+//ÖDEV
+//Kullanıcı tarafından girilen iki şifrenin aynı olması durumunda login yapma, farklı ise hatalı demesi
+
+(function ValidationPassword() {
+  while (true) {
+    var password = Number(prompt("Şifrenizi giriniz: "));
+    var password2 = Number(prompt("Şifrenizi tekrar giriniz: "));
+    if (password==password2) {
+      break;
+    }
+    alert("Şifreler eşleşemedi. İki şifreyi de tekrar giriniz.")
+  }
+  alert("Oturum Açılıyor...");
+  document.write("Oturum Açıldı.")
+})()
